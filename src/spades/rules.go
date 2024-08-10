29e4620fd5144fa4d1d0e33cf1a836fdd5022f01
Anton Orlichenko <aorliche@gmail.com>
2024-08-10 00:03:13 -0500

    Player actions for spades

--- src/spades/rules.go
+++ src/spades/rules.go
@@ -1,6 +1,7 @@
 package spades
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
 )
@@ -21,7 +22,7 @@ const (
 type Action struct {
 	Verb Verb
 	Card Card
-	Tricks int
+	Bid int
 }
 
 type GameState struct {
@@ -60,20 +61,24 @@ func (card Card) ToStr() string {
     return fmt.Sprintf("%s of %s", card.RankStr(), card.SuitStr()) 
 }
 
-func (card Card) Beats(other Card, playerSuit int) bool {
+func (card Card) Beats(other Card, firstSuit int) bool {
 	if card.Suit() == SUIT_SPADES && other.Suit() != SUIT_SPADES {
 		return true
 	}
 	if card.Suit() != SUIT_SPADES && other.Suit() == SUIT_SPADES {
 		return false
 	}
-	if card.Suit() == other.Suit() && card.Rank() > other.Rank() {
+	if card.Suit() == firstSuit && other.Suit() != firstSuit {
 		return true
 	}
-	if card.Suit() == playerSuit && other.Suit() != playerSuit {
-		return true
+	if card.Suit() == firstSuit && other.Suit() == firstSuit {
+		return card.Rank() > other.Rank()
 	}
 	return false
+}
+
+func InitStack() Stack {
+	return Stack([4]Card{NO_CARD, NO_CARD, NO_CARD, NO_CARD})
 }
 
 func (stack Stack) Winner() int {
@@ -99,6 +104,20 @@ func (stack Stack) Winner() int {
 	return -1
 }
 
+func (stack Stack) Play(card Card) error {
+	if stack[3] != NO_CARD {
+		return errors.New("Stack already has 4 cards")
+	}
+	for i := 0; i < 4; i++ {
+		if stack[i] == NO_CARD {
+			stack[i] = card
+			return nil
+		}
+	}
+	// Unnecessary
+	return nil
+}
+
 func InitGameState() *GameState {
 	hands := [4][]Card{}
 	for i := 0; i < 4; i++ {
@@ -109,13 +128,56 @@ func InitGameState() *GameState {
 		player := i / 13
 		hands[player][i%13] = Card(deck[i])
 	}
-	bids := [4]int{}
+	bids := [4]int{-1,-1,-1,-1}
 	tricks := [4]int{}
 	return &GameState{
 		Hands: hands,
 		Bids: bids,
 		Tricks: tricks,
 		Attacker: 0,
-		Stack: Stack{},
+		Stack: InitStack(),
 	}
+}
+
+func (state *GameState) PlayerActions(player int) []Action {
+	acts := make([]Action, 0)
+	// This player has to bid
+	if state.bids[player] == -1 {
+		for i := 0; i <= 13; i++ {
+			acts.append(Action{Verb: BidVerb, Card: NO_CARD, Bid: i})
+		}
+		return acts
+	}
+	// Other players have to bid
+	for i := 0; i < 4; i++ {
+		if state.bids[i] == -1 {
+			return acts
+		}
+	}
+	// Play cards
+	for i := 0; i < 4; i++ {
+		j := (state.Attacker + i) % 4
+		if state.Stack[j] == NO_CARD {
+			// Not player's turn
+			if j != player {
+				return acts
+			}
+			firstSuit := state.Stack[0].Suit()
+			// Check whether we have suit
+			hasSuit := false
+			for _,c := range state.Hands[player] {
+				if c.Suit() == firstSuit {
+					hasSuit = true
+					break
+				}
+			}
+			// Add playable cards
+			for _,c := range state.Hands[player] {
+				if c.Suit() == firstSuit || !hasSuit {
+					acts = append(Action{Verb: PlayVerb, Card: c})
+				}
+			}
+		}
+	}
+	return acts
 }
